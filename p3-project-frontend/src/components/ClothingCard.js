046f3c9374ace7c11026a2289a41d80bfd30cda5
Graham <thegrahamprice@icloud.com>
2022-08-23 12:08:22 -0600

    Css and CC

--- p3-project-frontend/src/components/ClothingCard.js
+++ p3-project-frontend/src/components/ClothingCard.js
@@ -2,12 +2,12 @@ import React from "react";
 
 function ClothingCard({ piece }) {
   return (
-    <div>
-      <h1>{piece.title}</h1>
-      <p>{piece.price}</p>
-      <p>{piece.quantity}</p>
-      <p>{piece.brand}</p>
-      <img src={piece.url} alt=""></img>
+    <div id="clothing-hub">
+      <img id="hub-photo" src={piece.url} alt=""></img>
+      <h1 id="title">{piece.title}</h1>
+      <p id="clothing-information">
+        ${piece.price} BY {piece.brand}
+      </p>
     </div>
   );
 }

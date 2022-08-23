14fe2a02230cd36da2567e0264ebe8e1a5ff4741
Noah Licata <noahinbldr@gmail.com>
2022-08-23 12:31:31 -0600

    added navbar

--- p3-project-frontend/src/App.js
+++ p3-project-frontend/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Clothing from "./components/Clothing";
 import { useState, useEffect } from "react";
+import NavBar from "./components/Navbar";
 
 function App() {
   const [clothing, setClothing] = useState([]);
@@ -21,6 +22,7 @@ function App() {
 
   return (
     <div>
+      <NavBar />
       <Clothing clothing={clothing} onClothingClick={HandleClothingClick} />
     </div>
   );
